6c9fdae66c6101dfd2e76f509a80276d15e6f614
MarceloPrado <marcelinhprado@gmail.com>
2017-09-26 00:53:34 -0300

    Corrigindo bug ao buscar arestas, adicionado verificação de ao add arestas os dois vertices estiverem no grafo

--- src/main/java/GrafoJava/IdentificadorAresta.java
+++ src/main/java/GrafoJava/IdentificadorAresta.java
@@ -12,12 +12,10 @@ import GrafoThrift.Identificador;
  * @author MarceloPrado, Rhaniel Cristhian
  */
 public class IdentificadorAresta extends Identificador{
-    public int v1, v2, size;
-    public boolean isDirecionada;
-    
+    public int size;    
     
     public IdentificadorAresta(int v1, int v2, boolean isDirecionada, int size){
-        super(v1, v2, isDirecionada);
+        super(v1, v2, isDirecionada);        
         this.size = size;
     }
     
@@ -26,13 +24,13 @@ public class IdentificadorAresta extends Identificador{
         double a = 0.6180339887;
         double val = v1 * v2 * a;
         val = val - (int) val;
-        return (int)(val * this.size) + (isDirecionada ? 2 : 1);
+        return (int)(val * this.size) + (this.isDirecionada() ? 2 : 1);
     }
     
     @Override
     public boolean equals(Identificador id){
         if(id != null){
-            if(id.v1 == this.v1 && id.v1 == this.v2 && id.isDirecionada() == id.isDirecionada())
+            if(id.v1 == this.v1 && id.v2 == this.v2 && id.isDirecionada() == this.isDirecionada())
                 return true;
         }
         
